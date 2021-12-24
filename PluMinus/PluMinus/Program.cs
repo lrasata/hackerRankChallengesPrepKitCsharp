0a14982872e882794e7d13641c89575a18d757c9
lrasata <rmliantsoa@gmail.com>
2021-12-24 16:49:57 +1300

    PlusMinus challenge submitted Successfully

---
+++ PluMinus/PluMinus/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/*
+* Programming Challenge from HackerRank - PlusMinus
+* Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. 
+* Print the decimal value of each fraction on a new line with  places after the decimal.
+* 
+* Stdin : 6
+* Stdin : -4 3 -9 0 4 1
+* Result : 
+* 0.500000
+* 0.333333
+* 0.166667
+* 
+* Here the array has n=6 elements, 3 positive integers, 2 negative integers, and 1 zero
+* Result are 3/6 = 0.500000, 2/6 = 0.333333, 1/6 = 0.166667
+* 
+* Author : lrasata
+*/
+
+namespace PluMinus
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'plusMinus' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static bool IsNaturalNumberAndLessThan100(int n )
+        {
+            if ( n >= 0 || n <= 100)
+            {
+                return true;
+            } 
+            return false;
+        }
+
+        public static bool IsIntegerAndLessThan100(int n)
+        {
+            if (n>100 || n<-100)
+            {
+                return false;
+            }
+            return true;
+
+        }
+        public static void PlusMinus(List<int> arr, int n)
+        {
+            if (arr.Count != n)
+            {
+                Console.WriteLine("The size of your array should be equal to n");
+            } else
+            {
+                if (Result.IsNaturalNumberAndLessThan100(n))
+                {
+                    int positive = 0;
+                    int negative = 0;
+                    int zero = 0;
+                    foreach(int i in arr)
+                    {
+                        if (Result.IsIntegerAndLessThan100(i))
+                        {
+                            if (i > 0)
+                            {
+                                positive++;
+                            }
+                            if ( i < 0)
+                            {
+                                negative++;
+                            }
+                            if ( i == 0)
+                            {
+                                zero++;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("I should be and Inetegr between -100 and 100");
+                            return;
+                        }
+                    }
+                    decimal positiveRatio = positive / (decimal) n;
+                    decimal negativeRatio = negative / (decimal) n;
+                    decimal zeroRatio = zero / (decimal) n;
+
+                    Console.WriteLine(decimal.Round(positiveRatio, 6));
+                    Console.WriteLine(decimal.Round(negativeRatio, 6));
+                    Console.WriteLine(decimal.Round(zeroRatio, 6));
+
+                }
+                else
+                {
+                    Console.WriteLine("N should be between 0 and 1OO");
+                }
+            }
+
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.PlusMinus(arr, n);
+        }
+    }
+
+}
---
+++ PluMinus/PluMinus/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PluMinus")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PluMinus")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bf7cdcf9-d564-4b35-9f99-d70d0fe3ecc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
